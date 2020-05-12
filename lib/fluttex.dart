ccc44a5582729540f5739ffb67e4e2c2e7634380
LeonStaufer <leon@staufer.me>
2020-05-12 12:53:56 +0200

    removed unnecessary const keyword

--- lib/fluttex.dart
+++ lib/fluttex.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Fluttex {
-  static const MethodChannel _channel =
-      const MethodChannel('fluttex');
+  static const MethodChannel _channel = MethodChannel('fluttex');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
